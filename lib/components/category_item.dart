9a580608af892220bc9087971305781eb2e1c034
Gabriel Bastos <g.porto@unesp.br>
2020-11-19 22:27:00 -0300

    alterado o estilo do componente Item Categoria

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -10,6 +10,21 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(category.title),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        //borda arredondada.
+        borderRadius: BorderRadius.circular(15),
+        //cria um gradiente de cores.
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          //orientação.
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
     );
   }
 }
