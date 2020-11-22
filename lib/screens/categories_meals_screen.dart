d5c95fe6f3f1065b2840a35ac7873b44c1520807
Gabriel Bastos <g.porto@unesp.br>
2020-11-22 17:28:10 -0300

    rotas nomeadas

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-  const CategoriesMealsScreen(this.category);
   @override
   Widget build(BuildContext context) {
+/*
+    Vai pegar o dado da categoria a partir da navegação da rota.
+    Vai receber o argumento que foi passado por essa rota. 
+*/
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
