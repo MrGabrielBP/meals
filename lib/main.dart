4613570755feff971696c508c85c91a6b12f2c1a
Gabriel Bastos <g.porto@unesp.br>
2020-12-07 08:57:48 -0300

    criada rota para tela de detalhe da refeição

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import './screens/categories_screens.dart';
 import 'utils/app_routes.dart';
 
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
         // rota '/': inicial
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
     );
   }
