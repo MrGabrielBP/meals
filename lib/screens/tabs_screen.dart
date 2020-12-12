d8a7f61a55bd43657330f09606d86158611d58c6
MrGabrielBP <g.porto@unesp.br>
2020-12-12 20:00:33 -0300

    adicionado TabBar na parte inferior

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,39 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 import './categories_screens.dart';
 import './favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  void _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Usado para controle de abas.
-    return DefaultTabController(
-        length: 2, //quantas abas estão sendo definidas.
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Vamos Cozinhar?'),
-            //parte inferior do Scaffold.
-            //TabBar
-            bottom: TabBar(
-              tabs: [
-                //Item do TabBar
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categorias',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favoritos',
-                )
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'],
+        ),
+      ),
+      //Menu lateral.
+      body: _screens[_selectedScreenIndex]['screen'],
+      //cria uma tabbar na parte inferior.
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        //funçao para item selecionado. Retorna o index desse item.
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        //para saber qual item está selecionado.
+        currentIndex: _selectedScreenIndex,
+        //type: BottomNavigationBarType.shifting,
+        //    animação ao mudar. Padrão: fixed.
+        items: [
+          //item do BottomNavigationBar.
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
           ),
-          //creates a view with one child per tab.
-          body: TabBarView(
-            children: [
-              CategoriesScreen(),
-              FavoriteScreen(),
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favoritos',
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
+
+/*
+//Usado para controle de abas.
+return DefaultTabController(
+  length: 2, //quantas abas estão sendo definidas.
+  child: Scaffold(
+    appBar: AppBar(
+      title: Text('Vamos Cozinhar?'),
+      //parte inferior do Scaffold.
+      //TabBar
+      bottom: TabBar(
+        tabs: [
+          //Item do TabBar
+          Tab(
+            icon: Icon(Icons.category),
+            text: 'Categorias',
+          ),
+          Tab(
+            icon: Icon(Icons.star),
+            text: 'Favoritos',
+          )
+        ],
+      ),
+    ),
+    //creates a view with one child per tab.
+    body: TabBarView(
+      children: [
+        CategoriesScreen(),
+        FavoriteScreen(),
+      ],
+    ),
+  ),
+);
+*/
